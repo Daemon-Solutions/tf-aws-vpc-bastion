ab731104ba46b0865536497983a293de95332a11
Liam Bennett <liam@bashton.com>
2016-09-13 15:52:35 +0100

    Adding the internet gateway to the public route table.

--- 20-vpc.tf
+++ 20-vpc.tf
@@ -51,8 +51,12 @@ resource "aws_subnet" "public" {
 }
 
 resource "aws_route_table" "public" {
-  depends_on = ["aws_internet_gateway.igw"]
   vpc_id     = "${aws_vpc.vpc.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.igw.id}"
+  }
 
   tags {
     Name        = "${var.name}-${var.envname}-public"
